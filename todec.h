b483a769084fbd0f59c208732e6a932692631eaa
Dariyastaa <154438637+Dariyastaa@users.noreply.github.com>
2023-12-22 16:01:56 +0300

    Update todec.h
    Убраны повторения include

--- todec.h
+++ todec.h
@@ -1,11 +1,5 @@
 #ifndef TODEC_H
 #define TODEC_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-#include <string.h>
-#include <ctype.h>
 
 long todec(char *num, int *base); 
 
